e530e07af3ce4d55531ef3d36a9d5a6653888dc7
Moritz Künzl <s4mokuen@uni-trier.de>
2021-12-04 11:42:29 +0100

    added scripts to relabel & parse primer

--- example/parse-by-primer.py
+++ example/parse-by-primer.py
@@ -1,26 +1,30 @@
+import glob
+import os
 import re
-from pathlib import Path
 
-input_file = Path('/Users/moritzkunzl/Documents/GitHub/metabar-coding/example/paired/fasta-quality/1_734_S1_paired.assembled_q30.fasta')
+input_dir = 'paired/fasta-quality'
+
+output_dir = 'trimmed'
+
+os.makedirs(output_dir, exist_ok=True)
 
 forward_primer = 'ACACCGCCCGTCACCCT'
 reverse_primer = 'GGGCGGTGT'
 header = ''
 
-file_reader = open(input_file, 'r')
-print(input_file)
-
-output_file = input_file.with_name(input_file.stem + '_matchPrimer' + input_file.suffix)
-file_writer = open(output_file, 'w')
-print(output_file)
-
-for line in file_reader:
-    if re.search(rf'^{forward_primer}.*{reverse_primer}$', line):
-        file_writer.write(header)
-        trimmed_line = line.removeprefix(forward_primer).removesuffix(reverse_primer)
-        file_writer.write(trimmed_line)
-        pass
-    header = line
-
-file_reader.close()
-file_writer.close()
+for filename in glob.iglob(input_dir + '/*_paired.assembled_q30.fasta'):
+    with open(filename, 'r') as file_reader:
+        print(filename)
+        output_file = filename.replace('_paired.assembled_q30.fasta', '_paired.assembled_q30_matchPrimer.fasta')\
+            .replace(input_dir, output_dir)
+        with open(output_file, 'w') as file_writer:
+            print(output_file)
+            for line in file_reader:
+                if re.search(rf'^{forward_primer}.*{reverse_primer}$', line):
+                    file_writer.write(header)
+                    trimmed_line = line.removeprefix(forward_primer).removesuffix(reverse_primer)
+                    file_writer.write(trimmed_line)
+                    pass
+                header = line
+            file_writer.close()
+        file_reader.close()
---
+++ example/relabel-fastq.py
@@ -0,0 +1,20 @@
+import fileinput
+import glob
+import re
+
+# input_dir = input('Directory: ')
+input_dir = '/home/mkuen/git/metabar-coding/example/paired'
+
+for filename in glob.iglob(input_dir + '/*assembled.fastq'):
+    with fileinput.input(filename, inplace=1) as file_reader_writer:
+        line_counter = 1
+        for line in file_reader_writer:
+            if re.search(rf'^>', line):
+                file_id = filename.replace(input_dir + '/', '')\
+                    .replace('_S1_paired.assembled_q30_matchPrimer.fasta', '.')
+                print(line.replace(line, '>' + file_id + str(line_counter)))
+                line_counter += 1
+                pass
+            else:
+                print(line.replace('\n', ''))
+        file_reader_writer.close()
