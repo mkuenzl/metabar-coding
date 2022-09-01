87a80aa576f647687826b63cc823c119099f3036
Moritz Künzl <mo.kuenzl@gmail.com>
2022-09-01 16:16:15 +0200

    fixed removal of reverse_primer, due to not thinking about linebreaks

--- pipeline/parse_primer.py
+++ pipeline/parse_primer.py
@@ -6,7 +6,6 @@ import fileinput
 
 
 def parse(forward_primer: str, reverse_primer: str):
-
     os.makedirs(folder_structure.FASTA_QUALITY_TRIMMED, exist_ok=True)
 
     header = ''
@@ -14,7 +13,7 @@ def parse(forward_primer: str, reverse_primer: str):
     for filename in glob.iglob(folder_structure.FASTA_QUALITY + '/*_paired.assembled_q30.fasta'):
         with open(filename, 'r') as file_reader:
             print(filename)
-            output_file = filename.replace('_paired.assembled_q30.fasta', '_paired.assembled_q30_matchPrimer.fasta')\
+            output_file = filename.replace('_paired.assembled_q30.fasta', '_paired.assembled_q30_matchPrimer.fasta') \
                 .replace(folder_structure.FASTA_QUALITY, folder_structure.FASTA_QUALITY_TRIMMED)
             with open(output_file, 'w') as file_writer:
                 print(output_file)
@@ -27,12 +26,21 @@ def parse(forward_primer: str, reverse_primer: str):
                 for line in file_reader:
                     if re.search(rf'^{forward_primer}.*{reverse_primer}$', line):
                         file_writer.write(header)
-                        trimmed_line = line.removeprefix(forward_primer).removesuffix(reverse_primer)
-                        file_writer.write(trimmed_line)
+                        lineWithoutPrefix = line[len(forward_primer):]
+                        # lineWithoutPrefix = line.removeprefix(forward_primer)
+                        if lineWithoutPrefix.endswith("\n"):
+                            lineWithoutPreSuffix = lineWithoutPrefix[:-len(reverse_primer + "\n")]
+                            # lineWithoutPreSuffix = lineWithoutPrefix.removesuffix(reverse_primer + "\n")
+                            file_writer.write(lineWithoutPreSuffix + "\n")
+                        else:
+                            lineWithoutPreSuffix = lineWithoutPrefix[:-len(reverse_primer)]
+                            # lineWithoutPreSuffix = lineWithoutPrefix.removesuffix(reverse_primer)
+                            file_writer.write(lineWithoutPreSuffix)
                         pass
                     header = line
                 file_writer.close()
             file_reader.close()
+
 
 def label():
     for filename in glob.iglob(folder_structure.FASTA_QUALITY_TRIMMED + '/*.fasta'):
@@ -40,8 +48,9 @@ def label():
             line_counter = 1
             for line in file_reader_writer:
                 if re.search(rf'^>', line):
-                    file_id = filename.replace(folder_structure.FASTA_QUALITY_TRIMMED + '/', '')\
+                    file_id = filename.replace(folder_structure.FASTA_QUALITY_TRIMMED + '/', '') \
                         .replace('_paired.assembled_q30_matchPrimer.fasta', '.')
+                    # identifier = file_id[file_id.find('_'):]
                     print(line.replace(line, '>' + file_id + str(line_counter)))
                     line_counter += 1
                     pass
